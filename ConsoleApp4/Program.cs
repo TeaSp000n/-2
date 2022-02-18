57052ca99fe783b8755e6988a366e6062194f077
Олександр <@DESKTOP-S660HFA>
2022-02-18 13:53:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Заповнiть анкету");
+
+            User someUser = new User();
+
+            Console.WriteLine("Логiн");
+            someUser.Login = Console.ReadLine();
+            Console.WriteLine("Name");
+            someUser.Name = Console.ReadLine();
+            Console.WriteLine("SureName");
+            someUser.SureName = Console.ReadLine();
+            Console.WriteLine("Years");
+            someUser.Years = int.Parse(Console.ReadLine());
+            Console.WriteLine("Date of writing");
+            someUser.DateOf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(someUser.GetUser());
+
+
+
+
+            Console.WriteLine("Зроблено");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp4/User.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class User
+   
+    {
+
+        private string login;
+        private string name;
+        private string sureName;
+        private int years;
+        private int dateOf;
+
+
+        public string Login 
+        { 
+            get
+            {
+                return login;
+
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+        public string Name 
+        {
+            get
+            {
+                return name;
+
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string SureName 
+        {
+            get
+            {
+                return sureName;
+
+            }
+
+            set
+            {
+                sureName = value;
+            }
+
+        }
+        public int Years
+        { 
+            get
+            {
+                return years;
+
+            }
+
+            set
+            {
+                years = value;
+            }
+        }
+        public int DateOf
+        {
+            get
+            {
+                return dateOf;
+
+            }
+
+            set
+            {
+                dateOf = value;
+            }
+        }
+        public string GetUser()
+        {
+            return $"Login: {login}; Name: {name}; SureName: {sureName}; Years: {years}; DateOf: {dateOf}";
+        }
+
+
+
+    }
+}
